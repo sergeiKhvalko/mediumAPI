b9716df3328e3014dfeaea496e6d2a899e188fd1
sergei khvalko <falko.serega2016@yandex.ru>
2023-07-24 12:18:32 +0300

    feat: getting tags from postgres

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { TagEntity } from './tag.entity'
 
 @Injectable()
 export class TagService {
-	findAll(): string[] {
-		return ['react', 'vue', 'angular']
+	constructor(
+		@InjectRepository(TagEntity)
+		private readonly tagRepository: Repository<TagEntity>,
+	) {}
+	async findAll(): Promise<TagEntity[]> {
+		return await this.tagRepository.find()
 	}
 }
